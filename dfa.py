3c6d70a5ae96af0fcf551b1e1acd02114d3d0e26
joaogpd <joaogdrumond@hotmail.com>
2023-08-01 23:22:11 -0300

    Create dfa.py

---
+++ dfa.py
@@ -0,0 +1,59 @@
+"""
+This class implements a DFA structure. States, in Q, is a list of the possible states of the
+DFA. Sigma is the alphabet that the DFA can accept. If a symbol not in the alphabet is provided
+to the delta_hat function, it will issue an error message and abort execution. 
+delta is a dictionary with keys of the model (state, symbol), and the content referenced by the
+key is the state p resulting of delta(state, symbol). F is a list that contains the accepting states of the DFA, and q0 is the start state, being only a one-character string.
+The delta_hat function takes in a string and evaluates whether it belongs to the language of the
+DFA or not. The delta_hat_hat function is just iterating over a list of strings, for 
+ease of use with larger amounts of data/strings.
+"""
+class DFA:
+  def __init__(self, Q: list, Sigma: list, delta: dict, F: list, q0: str):
+    self.Q = Q
+    self.Sigma = Sigma
+    self.delta = delta
+    self.F = F
+    self.q0 = q0
+
+  def delta_hat(self, string: str):
+    current_state = self.q0
+    for symbol in string:
+      if symbol not in self.Sigma:
+        print(f"String contains symbol not in alphabet {self.Sigma}")
+        return
+      current_state = self.delta[(current_state, symbol)]
+    if current_state in self.F:
+      print(f"{string} belongs to the language of the DFA.")
+    else:
+      print(f"{string} does not belong to the language of the DFA.")
+
+  def delta_hat_hat(self, string_list: list):
+    for string in string_list:
+      self.delta_hat(string)
+
+
+"""
+Example usage of the class. This DFA accepts only strings ended by 01 or 00.
+"""
+def main():
+  DFA_1 = DFA(
+      ["A", "B", "C", "D"], ["0", "1"], {
+          ("A", "0"): "B",
+          ("A", "1"): "A",
+          ("B", "0"): "C",
+          ("B", "1"): "D",
+          ("C", "0"): "C",
+          ("C", "1"): "D",
+          ("D", "0"): "B",
+          ("D", "1"): "A"
+      }, ["C", "D"], "A")
+
+  string_list = [
+      "00", "01", "0010", "0001001", "000000", "001110", "0110110", "00001"
+  ]
+  DFA_1.delta_hat_hat(string_list)
+
+
+if __name__ == "__main__":
+  main()
